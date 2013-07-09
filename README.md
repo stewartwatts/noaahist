cb13049b08800cd9fe2358432872e1c22a256135
Stewart Watts <stewartwatts@gmail.com>
2013-07-09 00:53:46 -0600

    begin logic depending on args in noaahist.py; classes for req/resp; arg file parsing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-API to get historical NOAA weather station data by nearest zip code or latitude and longitude coordinates. 
+OVERVIEW: API to get historical NOAA weather data from the NOAA station nearest a zip code or latitude and longitude coordinates. 
+
+DEPENDS: *NIX curl, pyzipcode (if you pass zip codes instead of longitude and latitude)
+
+DATA: ftp://ftp.ncdc.noaa.gov/pub/data/noaa/     (OLD: NOAA Quality Controlled Local Climatological Data)
+
+COVERAGE: July 1996 - present
+
+FIELDS: 
+
+SPEED: The NOAA files this script downloads are large even when compressed.  Each API call will take a long time, particularly for long date ranges.  It is recommended to pass all the locations needed for a given month in one call to avoid redownloading large files multiple times.  
--- noaahist.py
+++ noaahist.py
@@ -1,36 +1,65 @@
-#!/usr/bin/env python
+#!/usr/bin/env python  
 
 import sys
 import urllib2
 import datetime as dt
 import argparse
+from subprocess import Popen, PIPE
 from math import radians, cos, sin, asin, sqrt
 
-weather_keys = {
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    }
+class WeatherDataRequest(object):
+    def __init__(self, dates, lat, lon, flds, freq):
+        self.dates = {date: None for date in dates}
+        self.lat = lat
+        self.lon = lon
+        self.flds = flds
+        self.freq = freq
+        self.get_stns_by_date()
 
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    km = 6367 * c
-    return km * 0.621371
+    def get_stns_by_date(self, stn_months):
+        pass
+
+class WeatherDataResponse(object):
+    def __init__(self, req_list):
+        self.req_list = req_list
+        self.fields = set(map(+, []))
+
+NOAA_fields = {
+    'STN':   [0, 6],         # Station number (WMO/DATSAV3 number)
+    'WBAN':  [7, 12],        # WBAN number
+    'YEAR':  [14, 18],       # year
+    'MODA':  [18, 22],       # month and day
+    'TEMP':  [24, 30],       # mean temperature in degrees F. Missing: 9999.9
+    'DEWP':  [35, 41],       # Mean dew point
+    'SLP':   [46, 52],       # Mean sea level pressure, millibars
+    'STP':   [57, 63],       # Mean station pressure
+    'VISIB': [68, 73],       # Mean visibility in miles. Missing = 999.9
+    'WDSP':  [78, 83],       # Mean wind speed in knots. Missing = 999.9
+    'MXSPD': [88, 93],       # Max sustained wind speed in knots.
+    'GUST':  [85, 100],      # Max wind gust in knots.
+    'MAX':   [102, 108],     # Max temp in F
+    'MIN':   [110, 116],     # Min temp in F
+    'PRCP':  [118, 123],     # Total precipitation in inches
+    'SNDP':  [125, 130],     # Snow depth in inches
+    'FRSHTT': [132, 138],    # Codes, 1=yes, 0=no:
+                             # Fog ('F' - 1st digit).
+                             # Rain or Drizzle ('R' - 2nd digit).
+                             # Snow or Ice Pellets ('S' - 3rd digit).
+                             # Hail ('H' - 4th digit).
+                             # Thunder ('T' - 5th digit).
+                             # Tornado or Funnel Cloud ('T' - 6th digit).
+}
+
+NOAA_fields = {
+    '':'',
+    '':'',
+    '':'',
+    '':'',
+    '':'',
+    '':'',
+    '':'',
+    '':'',    
+}
 
 def date_action():
     class DateArgsAction(argparse.Action):
@@ -91,13 +120,84 @@ def lons_action():
             setattr(args, self.dest, values)
     return LonArgsAction
 
+def wkey_action(weather_keys):
+    class WkeyArgsAction(argparse.Action):
+        def __call__(self, parser, args, values, option_string=None):
+            if any(val not in weather_keys for val in values):
+                msg = '"lons" longitude floats must be in range [-180., 180.]'
+                raise argparse.ArgumentTypeError(msg)
+            setattr(args, self.dest, values)
+    return WkeyArgsAction
+
 def datestr_to_dt(datestr):
     return dt.date(*map(int, [datestr[:4], datestr[4:6], datestr[6:8]]))
 
-def main(args):
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    author: Michael Dunn
+    link: http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    km = 6367 * c
+    return km * 0.621371
+
+def coords_from_zip(zipcode):
+    pass
+
+def parse_infile_line(line):
+    [loc, dates, flds] = line.strip().split("|")
+    try:
+        [lat, lon] = loc.split(',')
+    except ValueError:
+        lat, lon = coords_from_zip(loc)
+    try:
+        [sd, ed] = map(datestr_to_dt, dates.split(','))
+    except ValueError:
+        sd = ed = datestr_to_dt(dates)
+    flds = flds.split(',')
+    return [sd, ed, lat, lon] + map(lambda x: NOAA_fields[x], flds)
+
+
+def main(args, update_stations=True):
+    if update_stations:
+        stns_url = 'ftp://ftp.ncdc.noaa.gov/pub/data/inventories/ISH-HISTORY.TXT'
+        stns_path = 'static/ISH-HISTORY.TXT'
+        if not os.path.exists(stns_path):
+            print "Downloading NOAA stations list to %s ..." % stns_path
+            with open(stns_path, "w") as f:
+                p1 = Popen(["curl", stns_url], stdout=PIPE)
+                # US stations only
+                p2 = Popen(["grep", "US US"], stdin=p1.stdout, stdout=PIPE)
+                # kill lines with no lat/long or no WBAN code
+                p3 = Popen(["grep", "-ve", "99999"], stdin=p2.stdout, stdout=PIPE)
+                f.write(p3.communicate()[0])
+    
+    # get longitude and latitude of all requested locations
+    lons, lats = [], []
     if args.lats or args.lons:
         assert len(args.lats) == len(args.lons), "number of latitude args must equal number of longitude args"
-    
+        lats += args.lats
+        lons += args.lons
+
+    if args.zips:
+        from pyzipcode import ZipCodeDatabase
+        zcdb = ZipCodeDatabase()
+        for _zip in args.zips:
+            zc = zcdb[int(_zip)]
+            lats.append(zc.latitude)
+            lons.append(zc.longitude)
+
+    # quality controlled local climatological data
+    qcd_url = 'http://cdo.ncdc.noaa.gov/qclcd_ascii/'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='PROG')
@@ -109,15 +209,20 @@ if __name__ == "__main__":
                         help='one or more latitudes')
     parser.add_argument('--lons', nargs='+', action=lons_action(), type=float,
                         help='one or more longitudes (in same order as latitudes if multiple)')
-    parser.add_argument('-v', '--val', nargs='+',
-                        help='keys for weather values to return (see weather_keys dict in noaahist.py)')
+    parser.add_argument('-f', '--flds', nargs='+',
+                        help='weather value keys for desired data (see weather_keys dict in noaahist.py)')
+    parser.add_argument('--hrly', action='store_true',
+                        help='return data with hourly frequency instead of daily')
     parser.add_argument('-n', '--no-stn-data', action='store_true', 
                         help='exclude station metadata for returned values')
-    parser.add_argument('-p', '--ret-py-objs', action='store_true', 
-                        help='return tuples instead of a string of pipe-delimeted time-series lines')
-    parser.add_argument('-o', '--out-file', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+    parser.add_argument('-i', '--infile', nargs'?', type=argparse.FileType('r'),
+                        help='pipe-delimited file with fmt: zip OR lat,lon | date OR startdate,enddate | field1,field2,...')
+    parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
                         help='direct output to a file')
     args = parser.parse_args()
-    print "VIEWING ARGS"
-    print args
+    #print "VIEWING ARGS"
+    #print args
+
     main(args)
+
+

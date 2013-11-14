0b213721dad36d428092b10d88493456fcaca526
stewartwatts <stewartwatts@gmail.com>
2013-11-14 14:07:37 -0500

    basic functions to explore stations near a location now running in explore_stations.py

--- explore_stations.py
+++ explore_stations.py
@@ -8,15 +8,20 @@ for _id in flds_by_stn:
         if flds_by_stn[_id][fld]:
             stns[_id]['flds'].append(fld)
 
-def view_stns_near_lat_lon(latitude, longitude, year, N=20):
+def view_stns_near_lat_lon(latitude, longitude, year, N=20, id_filter=None):
     """
     latitude: float -> latitude of weather location
     longitude: negative float, coerced to negative if positive -> longitude of weather location
     year: int -> the year of desired weather data
     N: int -> number of desired results
+    id_filter: function that returns station ids filtered on some condition
 
     displays information about the N closest stations
     """
+    latitude = float(latitude)
+    longitude = float(longitude)
+    if longitude > 0.:
+        longitude = -1. * longitude
     lines = [" ".join(['   ', "Station ID".ljust(12), "Station Name".ljust(30), "    ", "Dist. (miles)"]) + "\n"]
     def show_stn(stn_id, rank):
         d = stns[stn_id]
@@ -27,17 +32,39 @@ def view_stns_near_lat_lon(latitude, longitude, year, N=20):
                              str(round(haversine(latitude, longitude, stns[stn_id]['lat'], stns[stn_id]['lon']), 1))]) + "\n"
         return info_str
     stn_ids = [_id for _id in stns if int(stns[_id]['sd'].year) <= year and int(stns[_id]['ed'].year) >= year]
+    # optionally filter station ids
+    if id_filter:
+        stn_ids = id_filter(stn_ids)
     _ids = sorted(stn_ids, key=lambda _id: haversine(latitude, longitude, stns[_id]['lat'], stns[_id]['lon']))[:N]
     lines += [show_stn(_id, j) for j, _id in enumerate(_ids)]
-    print "".join(lines)
+    if len(lines) > 1:
+        print "".join(lines)
+    else:
+        print "No stations were found that matched."
 
-def view_stns_near_zip(zip, year, N=20):
+def view_stns_near_zip(zip, year, N=20, id_filter=None):
     try:
-        view_stns_near_lat_lon(*coords_from_zip(zip), year, N)
+        (lat, lon) = coords_from_zip(zip)
+        view_stns_near_lat_lon(lat, lon, year, N, id_filter)
     except ImportError:
         print "pyzipcode is not available -> try using view_stns_near_lat_lon()"
 
 def view_stns_with_fld(fld, latitude, longitude, year, N=20):
-    assert fld in ['TEMP','MIN','MAX','DEWP','DIR','SPD','GUS','PCP01','PCPXX','PCP06','PCP24','SD','SKC',
-                   'CLG','L','M','H','AW1','AW2','AW3','AW4','MW1','MW2','MW3','MW4','SLP','STP','ALT','VSB','W',]
+    valid_flds = ['TEMP','MIN','MAX','DEWP','DIR','SPD','GUS','PCP01','PCPXX','PCP06','PCP24','SD','SKC',
+                  'CLG','L','M','H','AW1','AW2','AW3','AW4','MW1','MW2','MW3','MW4','SLP','STP','ALT','VSB','W',]
+    assert fld in valid_flds, "the fld argument is not among the valid fields: %s" % ",".join(valid_flds)
+    def filter_ids(stn_ids):
+        return [x for x in stn_ids if fld in stns[x]['flds']]
+    view_stns_near_lat_lon(latitude, longitude, year, N, filter_ids)
     
+def view_stns_with_fld_zip(fld, zip, year, N=20):
+    try:
+        (lat, lon) = coords_from_zip(zip)
+        view_stns_with_fld(fld, lat, lon, year, N, id_filter)
+    except ImportError:
+        print "pyzipcode is not available -> try using view_stns_near_lat_lon()"
+
+if __name__ == "__main__":
+    print "This module is for interactive exploration."
+    print "Find weather stations near a location, optionally requesting that a certain field is likely to be present."
+    print "There are no guarantees that the data for any field will exist.  Data observations are sparse in many cases."
